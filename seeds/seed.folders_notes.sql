03e1f72c1b43813ebba7f15d1ec3be46a8a80462
Peter Rufo <peter@peterrufo.com>
2020-03-10 10:19:31 -0400

    api

---
+++ seeds/seed.folders_notes.sql
@@ -0,0 +1,30 @@
+TRUNCATE folders, notes RESTART IDENTITY CASCADE;
+
+INSERT INTO folders(folder_name)
+VALUES
+  ('First folder'),
+  ('Second folder'),
+  ('Third folder');
+
+INSERT INTO notes(note_name, content, folder_id)
+VALUES
+  ('First note',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+  1
+  ),
+  ('Second note',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+  2
+  ),
+  ('Third note',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+  3
+  ),
+  ('Fourth note',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+  2
+  ),
+  ('Fifth note',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+  1
+  );
